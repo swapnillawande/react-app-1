022a8dde7a2e723c4d785918d2d0204be8aa6f3f
swapnil <swapnillawande2001@gmail.com>
2024-08-29 11:59:22 +0530

    first commit1

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
-import {EXAMPLES} from './data.js';
+import { EXAMPLES } from './data.js';
 
 function App() {
- 
-  const [ text, setText] = useState('components');
-  
-  function handleSelect(selectedButton){
+
+  const [text, setText] = useState();
+
+  function handleSelect(selectedButton) {
     setText(selectedButton);
   }
 
@@ -21,46 +21,54 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts!</h2>
           <ul>
-            <CoreConcept 
+            {/* {CORE_CONCEPTS.map((conceptItem) => <CoreConcept {...conceptItem}/> )} */}
+            <CoreConcept
               title={CORE_CONCEPTS[0].title}
-              description = {CORE_CONCEPTS[0].description}
+              description={CORE_CONCEPTS[0].description}
               img={CORE_CONCEPTS[0].image}
             />
-            <CoreConcept 
+            <CoreConcept
               title={CORE_CONCEPTS[1].title}
-              description = {CORE_CONCEPTS[1].description}
+              description={CORE_CONCEPTS[1].description}
               img={CORE_CONCEPTS[1].image}
             />
-            <CoreConcept 
+            <CoreConcept
               title={CORE_CONCEPTS[2].title}
-              description = {CORE_CONCEPTS[2].description}
+              description={CORE_CONCEPTS[2].description}
               img={CORE_CONCEPTS[2].image}
-            />            
-            <CoreConcept 
+            />
+            <CoreConcept
               title={CORE_CONCEPTS[3].title}
-              description = {CORE_CONCEPTS[3].description}
+              description={CORE_CONCEPTS[3].description}
               img={CORE_CONCEPTS[3].image}
             />
- 
-            </ul>
+
+          </ul>
         </section>
         <section id='examples'>
-            <h2>Examples</h2>
-            <menu>
-            <TabButton onSelect ={() => handleSelect('components')}>Components</TabButton>
-            <TabButton onSelect ={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton onSelect ={() => handleSelect('props')}>Props</TabButton>
-            <TabButton onSelect ={() => handleSelect('state')}>State</TabButton>
-            </menu>
+          <h2>Examples</h2>
+          <menu>
+            <TabButton isSelected={text === 'components'} onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton isSelected={text === 'jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton isSelected={text === 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton isSelected={text === 'state'} onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+
+          {!text ? <p>Please select a topic.</p> :
+
+
             <div id='tab-content'>
-                <h3>{EXAMPLES[text].title}</h3>
-                <p>{EXAMPLES[text].description}</p>
-                <pre>
-                  <code>
-                    {EXAMPLES[text].code}
-                  </code>
-                </pre>
+              <h3>{EXAMPLES[text].title}</h3>
+              <p>{EXAMPLES[text].description}</p>
+              <pre>
+                <code>
+                  {EXAMPLES[text].code}
+                </code>
+              </pre>
             </div>
+
+          }
+
         </section>
 
       </main>
